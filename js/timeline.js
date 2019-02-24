83a6dd7a518eeb9bd64c07cda4040b981cb49fa3
Joe Hinkley <joehhinkley@gmail.com>
2019-02-24 15:49:16 -0800

    added the tubeless project

--- js/timeline.js
+++ js/timeline.js
@@ -10,10 +10,9 @@ google.charts.load("current", {packages:["timeline"]});
     dataTable.addColumn({ type: 'date', id: 'Start' });
     dataTable.addColumn({ type: 'date', id: 'End' });
     dataTable.addRows([
-      [ 'Education', 'Bellevue College', new Date(2015, 3, 6), new Date(2015, 5, 13) ],
-      [ 'Education', 'Edmonds Community College', new Date(2015, 5, 28), new Date(2015, 11, 24) ],
       [ 'Education', 'Shoreline Community College', new Date(2016, 0, 3), new Date(2017, 5, 13) ],
       [ 'Education', 'Edmonds Community College', new Date(2017, 8, 23), new Date(2018, 5, 13) ],
+      [ 'Education', 'General Assembly', new Date(2018, 11, 10), new Date(2019, 2, 21) ],
       [ 'Internships', 'Destination Marketing', new Date(2015, 9, 14), new Date(2017, 6, 8) ],
       [ 'Internships', 'Clatter & Din', new Date(2017, 0, 3), new Date(2017, 6, 14) ],
       [ 'Internships', 'SkyMuse Studios', new Date(2017, 5, 7), new Date(2017, 9, 11) ],
